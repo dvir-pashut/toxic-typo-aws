5d5dcb0aac3132c1b6f28295e95ddaf0227623bb
dvir.p1999 <dvir.p1999@gmail.com>
2023-01-05 16:37:24 +0000

    last fix

--- init.sh
+++ init.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+ec2_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+# remove from target group
+aws elbv2 deregister-targets \
+--target-group-arn arn:aws:elasticloadbalancing:eu-west-3:644435390668:targetgroup/dvirtg/8cdc359352c43e17 \
+--targets Id="${ec2_id}"
+
+sleep 3
+
+
 docker rm -f tox-app
-
 docker pull 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
-
 docker run -d -p 8080:8080 --name tox-app 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
+
+sleep 7
+
+curl -X POST -F "name=${ec2_id}" "http://localhost/api/name"
+
+
+# add back to target group
+aws elbv2 register-targets \
+--target-group-arn arn:aws:elasticloadbalancing:eu-west-3:644435390668:targetgroup/dvirtg/8cdc359352c43e17 \
+--targets Id="${ec2_id}"

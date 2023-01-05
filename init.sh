5afbcb5067dde0a5591ed3c95d39f4957479e277
dvir.p1999 <dvir.p1999@gmail.com>
2023-01-05 17:11:11 +0000

    last fix

--- init.sh
+++ init.sh
@@ -9,7 +9,6 @@ aws elbv2 deregister-targets \
 
 sleep 3
 
-
 docker rm -f tox-app
 docker pull 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
 docker run -d -p 8080:8080 --name tox-app 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest

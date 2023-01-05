0bbae15516fc4122253084b4b507b4409ece2acf
dvir.p1999 <dvir.p1999@gmail.com>
2023-01-05 13:08:44 +0000

    first build

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ docker rm -f tox-app
 
 docker pull 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
 
-docker run -d -p 80:8080 --name tox-app 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
+docker run -d -p 8080:8080 --name tox-app 644435390668.dkr.ecr.eu-west-3.amazonaws.com/dvir-toxictypo:latest
